25578ed4efc80249fe6c9010b73c60a46c5ff7c9
ChuckRBell <chuckrbell@gmail.com>
2018-06-20 20:33:17 -0500

    This is my first GitHub commit

---
+++ Hello Universe/Hello Universe.cpp
@@ -0,0 +1,15 @@
+// Simple C++ program to display "Hello Universe"
+
+// Header file for input output functions
+#include <iostream>
+
+using namespace std;
+
+// main function -
+// where the execution of program begins
+int main()
+{
+    // prints Hello universe
+    cout << "Hello universe!" << endl;
+    return 0;
+}
